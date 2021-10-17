9bbefcf7b95b2d238051372ce9db809eeeb85efd
Adrien Degliame <adidf-web@laposte.net>
2021-10-17 12:25:42 +0200

    Generate Function

--- src/components/Function.tsx
+++ src/components/Function.tsx
@@ -1,10 +1,18 @@
 import React, { FC, useState } from "react";
 import { Box, Container, Select } from "@chakra-ui/react";
+import { Provider, Node } from "@nteract/mathjax";
 
-export type FunctionType = "afine" | "inverse" | "power";
+export type FunctionDef =
+  | { type: "afine"; coef: number; dec: number }
+  | { type: "power"; coef: number; dec: number; power: number }
+  | { type: "inverse"; coef: number; dec: number; power: number };
 
 export const Function: FC = () => {
-  const [functionType, setFunctionType] = useState<FunctionType>("afine");
+  const [func, setFunc] = useState<FunctionDef>({
+    type: "afine",
+    coef: 1,
+    dec: 0,
+  });
 
   return (
     <Box
@@ -19,13 +27,33 @@ export const Function: FC = () => {
       <Select
         bg="white"
         color="black"
-        onChange={(e) => setFunctionType(e.target.value as FunctionType)}
+        onChange={({ target: { value } }) =>
+          setFunc(
+            value === "power"
+              ? { type: "power", coef: 1, dec: 0, power: 2 }
+              : value === "inverse"
+              ? { type: "inverse", coef: 1, dec: 0, power: 1 }
+              : { type: "afine", coef: 1, dec: 0 }
+          )
+        }
       >
         <option value="afine">Afine : f(x) = ax + b</option>
-        <option value="inverse">Inverse : f(x) = a(1/x) + b</option>
         <option value="power">Power : f(x) = a(x**c) + b</option>
+        <option value="inverse">Inverse : f(x) = a(1/x**c) + b</option>
       </Select>
-      <Box></Box>
+      <Box>
+        <Provider>
+          <Node>
+            {func.type === "afine"
+              ? `f(x) = ${func.coef}x + ${func.dec}`
+              : func.type === "power"
+              ? `f(x) = ${func.coef}x^${func.power} + ${func.dec}`
+              : func.type === "inverse"
+              ? `f(x) = ${func.coef}{1 \\over x^${func.power}} + ${func.dec}`
+              : ""}
+          </Node>
+        </Provider>
+      </Box>
     </Box>
   );
 };
