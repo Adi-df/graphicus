5d6d2678d6a15298aa5671194a248f88965aa2e8
Adrien Degliame <adidf-web@laposte.net>
2021-10-17 09:39:38 +0200

    Create base layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
+import { Center, Flex, Spacer } from "@chakra-ui/react";
 import React from "react";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Flex w="100%">
+      <Spacer />
+      <Center w="40%"></Center>
+      <Spacer />
+      <Center w="40%"></Center>
+      <Spacer />
+    </Flex>
+  );
 }
 
 export default App;
