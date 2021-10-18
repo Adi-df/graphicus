ae584a45380604c7b4cc7eb6f3bfbe3ddd1bafe7
Adrien Degliame <adidf-web@laposte.net>
2021-10-18 18:57:43 +0200

    Change rotating from bool to number

--- src/components/Rotating.tsx
+++ src/components/Rotating.tsx
@@ -1,4 +1,12 @@
-import { Box, Switch, Text } from "@chakra-ui/react";
+import {
+  Box,
+  Flex,
+  Slider,
+  SliderFilledTrack,
+  SliderThumb,
+  SliderTrack,
+  Text,
+} from "@chakra-ui/react";
 import React, { FC, useState } from "react";
 
 export const Rotating: FC<{
@@ -6,7 +14,7 @@ export const Rotating: FC<{
   width?: string;
   margin?: string;
 }> = (props) => {
-  const [rotating, setRotating] = useState(false);
+  const [rotating, setRotating] = useState(0);
 
   return (
     <Box
@@ -16,8 +24,33 @@ export const Rotating: FC<{
       border="white 2px solid"
       borderRadius="2xl"
     >
-      <Text fontSize="3xl">Rotating :</Text>
-      <Switch onChange={() => setRotating((v) => !v)} />
+      <Text fontSize="3xl">Rotating speed :</Text>
+      <Flex>
+        <Slider
+          min={0}
+          max={10}
+          value={rotating}
+          onChange={(e) => setRotating(e)}
+        >
+          <SliderTrack>
+            <SliderFilledTrack />
+          </SliderTrack>
+          <SliderThumb />
+        </Slider>
+        <Text
+          marginLeft="15px"
+          borderRadius="full"
+          bg="white"
+          width="40px"
+          height="35px"
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+          color="black"
+        >
+          {rotating}
+        </Text>
+      </Flex>
     </Box>
   );
 };
